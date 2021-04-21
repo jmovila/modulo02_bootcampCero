ecd5dab90b92a5764fdac7886dfa34182019a83c
jorge <jmovilariza@gmail.com>
2021-04-21 22:19:10 +0200

    perro segunda version

--- perro.py
+++ perro.py
@@ -3,4 +3,17 @@ class Perro():
         self.nombre = nombre
         self.edad = edad
         self.peso = peso
+    
+    def ladrar(self):
+        if self.peso >=8:
+            print('GUAU, GUAU')
+        else:
+            print('guau, guau')
+            
+    def __str__(self):
+        return 'Soy el Perro {}'.format(self.nombre)
         
+salchicho = Perro('salchicho', 5, 9)
+lola = Perro('lola', 3, 2)
+salchicho.ladrar()
+lola.ladrar()
