d7b550069eefba25ba4552028511c370c167de55
jorge <jmovilariza@gmail.com>
2021-04-21 22:53:34 +0200

    tercera version perro

--- perro.py
+++ perro.py
@@ -1,3 +1,18 @@
+class Dog():
+    def __init__(self):
+        self.nombre = ''
+        self.edad = None
+        self.peso = None
+        
+    def ladrar(self):
+        if self.peso == None:
+            print('soy un fantasma')
+            
+        if self.peso >=8:
+            print('GUAU')
+        else:
+            print('ay, ay')
+
 class Perro():
     def __init__(self, nombre, edad, peso):
         self.nombre = nombre
@@ -13,7 +28,27 @@ class Perro():
     def __str__(self):
         return 'Soy el Perro {}'.format(self.nombre)
         
-salchicho = Perro('salchicho', 5, 9)
-lola = Perro('lola', 3, 2)
-salchicho.ladrar()
-lola.ladrar()
+class PerroAsistencia(Perro):
+    def __init__(self, nombre, edad, peso, amo):
+        Perro.__init__(self, nombre, edad, peso)
+        self.amo = amo
+        self.trabajando = False
+    def __str__(self):
+        return 'Perro de asistencia de {}'.format(self.amo)
+    def pasear(self):
+        print('{} ayudo a mi dueño {} a pasear'.format(self.nombre, self.amo))
+    def ladrar(self):
+        if self.trabajando:
+            print('shhh, no puedo ladrar')
+        else:
+            Perro.ladrar(self)
+            
+    def trabajando(self, valor = None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando = valor
+
+rocky = PerroAsistencia('rocky', 4, 8, 'Luke')
+              
+        
